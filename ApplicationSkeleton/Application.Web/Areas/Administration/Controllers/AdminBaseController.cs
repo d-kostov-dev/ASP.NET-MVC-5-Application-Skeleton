987c03958f3941a838d5aa3d631e5d6356888c15
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-16 20:24:39 +0200

    Admin Panel + Admin Base Controller

---
+++ ApplicationSkeleton/Application.Web/Areas/Administration/Controllers/AdminBaseController.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using Application.Common;
+    using Application.Data.Contracts;
+    using Application.Web.Controllers;
+    
+    [Authorize(Roles = GlobalConstants.AdminRole)]
+    public class AdminBaseController : BaseController
+    {
+        public AdminBaseController(IDataProvider provider)
+            : base(provider)
+        {
+        }
+    }
+}
