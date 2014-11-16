5e335fde86ce4706c31484cf89a0cc45b898804d
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-16 21:01:12 +0200

    Profile Details + Theme Change

---
+++ ApplicationSkeleton/Application.Web/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper.QueryableExtensions;
+using Application.Web.Models;
+
+namespace Application.Web.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        public ProfileController(IDataProvider provider)
+            : base(provider)
+        {
+        }
+
+        public ActionResult Details()
+        {
+            var profile =
+                this.Data.ApplicationUsers.All()
+                .Where(x => x.Id == this.CurrentUser.Id)
+                .Project().To<ProfileDetailsViewModel>()
+                .FirstOrDefault();
+
+            return View(profile);
+        }
+    }
+}
---
+++ ApplicationSkeleton/Application.Web/Models/Profile/ProfileDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Application.Web.Models
+{
+    using System;
+
+    using Application.Models;
+    using Application.Web.Infrastructure.Mapping;
+
+    using AutoMapper;
+
+    public class ProfileDetailsViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string TownName { get; set; }
+
+        public string Address { get; set; }
+
+        public int? ImageId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<ApplicationUser, ProfileDetailsViewModel>()
+                .ForMember(m => m.TownName, opt => opt.MapFrom(u => u.Town.Name));
+        }
+    }
+}
