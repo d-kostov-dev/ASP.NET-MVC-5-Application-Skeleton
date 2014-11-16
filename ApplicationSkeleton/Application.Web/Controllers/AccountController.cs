a59b14c3397561ecea9d2a0152ffd3d495dc5350
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-16 23:24:29 +0200

    Fixed Registration Bug

--- ApplicationSkeleton/Application.Web/Controllers/AccountController.cs
+++ ApplicationSkeleton/Application.Web/Controllers/AccountController.cs
@@ -92,7 +92,7 @@ namespace Application.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser() { UserName = model.Email, Email = model.Email, CreatedOn = DateTime.Now };
                 IdentityResult result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
