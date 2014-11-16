76604347fccf3a925220799bae2f9ba944d8e0a8
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-16 16:40:26 +0200

    BaseController And Ninject

---
+++ ApplicationSkeleton/Application.Web/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Application.Data.Contracts;
+    
+    public class BaseController : Controller
+    {
+        public BaseController(IDataProvider provider)
+        {
+            this.Data = provider;
+        }
+
+        public IDataProvider Data { get; private set; }
+    }
+}
--- ApplicationSkeleton/Application.Web/Controllers/HomeController.cs
+++ ApplicationSkeleton/Application.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Data.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,18 @@ using System.Web.Mvc;
 
 namespace Application.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        public HomeController(IDataProvider provider)
+            : base (provider)
+        {
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var pagesTitles = this.Data.InfoPages.All().Select(x => x.Title).ToList();
+
+            return Content(string.Join(", ", pagesTitles));
         }
     }
 }
