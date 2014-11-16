d01cf3ed6de5705fc37fd5a7cfd36f7940654842
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-16 22:14:08 +0200

    Edit User Profile

--- ApplicationSkeleton/Application.Web/Controllers/ProfileController.cs
+++ ApplicationSkeleton/Application.Web/Controllers/ProfileController.cs
@@ -1,14 +1,18 @@
-﻿using Application.Data.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using AutoMapper.QueryableExtensions;
-using Application.Web.Models;
-
-namespace Application.Web.Controllers
+﻿namespace Application.Web.Controllers
 {
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Application.Data.Contracts;
+    using Application.Web.Models;
+    using AutoMapper.QueryableExtensions;
+    using Application.Web.Models.Profile;
+    using System.IO;
+    using Application.Models;
+    using System;
+   
+    [Authorize]
     public class ProfileController : BaseController
     {
         public ProfileController(IDataProvider provider)
@@ -27,6 +31,65 @@ namespace Application.Web.Controllers
             return View(profile);
         }
 
+        public ActionResult Edit()
+        {
+
+            this.SetCountries();
+            this.SetTowns();
+
+            var profile =
+                this.Data.ApplicationUsers.All().Where(x => x.Id == this.CurrentUser.Id)
+                .Project().To<ProfileEditInputModel>()
+                .FirstOrDefault();
+
+            return this.View(profile);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProfileEditInputModel inputData)
+        {
+            if (inputData != null && ModelState.IsValid)
+            {
+                var profileToEdit = this.Data.ApplicationUsers.Find(this.CurrentUser.Id);
+
+                profileToEdit.UserName = inputData.UserName;
+                profileToEdit.Email = inputData.Email;
+                profileToEdit.FirstName = inputData.FirstName;
+                profileToEdit.LastName = inputData.LastName;
+                profileToEdit.ContactPhone = inputData.ContactPhone;
+                profileToEdit.Address = inputData.Address;
+                profileToEdit.TownId = inputData.TownId;
+                profileToEdit.DateOfBirth = inputData.DateOfBirth;
+
+                if (inputData.UploadedImage != null)
+                {
+                    using (var memory = new MemoryStream())
+                    {
+                        inputData.UploadedImage.InputStream.CopyTo(memory);
+                        var content = memory.GetBuffer();
+
+                        profileToEdit.Image = new Image
+                        {
+                            Content = content,
+                            FileExtension = inputData.UploadedImage.FileName.Split(new[] { '.' }).Last(),
+                            CreatedOn = DateTime.Now
+                        };
+                    }
+                }
+
+                this.Data.SaveChanges();
+
+                return RedirectToAction("Details");
+            }
+
+            this.SetCountries();
+            this.SetTowns();
+
+            return View(inputData);
+        }
+
         public ActionResult GetImage(int id)
         {
             var image = this.Data.Images.Find(id);
@@ -37,6 +100,25 @@ namespace Application.Web.Controllers
             }
 
             return File(image.Content, "image/" + image.FileExtension);
+        }
+
+        private void SetCountries()
+        {
+            var id = this.CurrentUser.Town != null ? this.CurrentUser.Town.CountryId : 1;
+
+            var items = new SelectList(this.Data.Countries.All().ToList(), "Id", "Name", id.ToString());
+
+            ViewBag.Countries = items;
+        }
+
+        private void SetTowns()
+        {
+            var countryId = this.CurrentUser.Town != null ? this.CurrentUser.Town.CountryId : 1;
+            var id = this.CurrentUser.TownId;
+
+            var items = new SelectList(this.Data.Towns.All().Where(x => x.CountryId == countryId).ToList(), "Id", "Name", id.ToString());
+
+            ViewBag.Towns = items;
         }
     }
 }
---
+++ ApplicationSkeleton/Application.Web/Models/Profile/ProfileEditInputModel.cs
@@ -0,0 +1,52 @@
+﻿namespace Application.Web.Models.Profile
+{
+    using System;
+    using System.Web;
+
+    using Application.Models;
+    using Application.Web.Infrastructure.Mapping;
+
+    using AutoMapper;
+    using System.ComponentModel.DataAnnotations;
+   
+    public class ProfileEditInputModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
+    {
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Display(Name = "Contact Phone")]
+        public string ContactPhone { get; set; }
+
+        [Display(Name = "Country")]
+        public int? CountryId { get; set; }
+
+        [Display(Name = "Town")]
+        public int? TownId { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name = "Profile Photo")]
+        public HttpPostedFileBase UploadedImage { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<ApplicationUser, ProfileEditInputModel>()
+                .ForMember(m => m.CountryId, opt => opt.MapFrom(u => u.Town.CountryId))
+                .IgnoreAllPropertiesWithAnInaccessibleSetter();
+        }
+    }
+}
